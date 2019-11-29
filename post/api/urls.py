27d4ae069b6bcba3f54eeb039465d7bbf1772f26
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-11-29 22:52:23 +0300

    Created Django Rest

---
+++ post/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from post.api.views import PostListAPIView
+
+urlpatterns = [
+    path('list', PostListAPIView.as_view(), name='list')
+]
---
+++ post/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    title = models.CharField(max_length=120)
+    content = models.TextField()
