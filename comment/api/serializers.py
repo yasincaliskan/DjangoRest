cac0cf04abe76ea4bf4bd5c50399a802bfffe4c7
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-12-02 21:32:34 +0300

    Comment pagination

--- comment/api/serializers.py
+++ comment/api/serializers.py
@@ -1,7 +1,6 @@
-from rest_framework.serializers import ModelSerializer
-
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from comment.models import Comment
-
 
 class CommentCreateSerializer(ModelSerializer):
     class Meta:
@@ -11,5 +10,22 @@ class CommentCreateSerializer(ModelSerializer):
     def validate(self, attrs):
         if(attrs["parent"]):
             if attrs["parent"].post != attrs["post"]:
-                return attrs
+                raise serializers.ValidationError("Something went wrong!")
+        return attrs
 
+class CommentListSerializer(ModelSerializer):
+    replies = SerializerMethodField()
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+    def get_replies(self, obj):
+        if obj.any_children:
+            return CommentListSerializer(obj.children(), many=True).data
+
+class CommentDeleteUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            'content'
+        ]
--- comment/api/views.py
+++ comment/api/views.py
@@ -1,6 +1,8 @@
-from rest_framework.generics import CreateAPIView
-from comment.api.serializers import CommentCreateSerializer
+from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView
+from comment.api.permissions import IsOwner
+from comment.api.serializers import CommentCreateSerializer, CommentListSerializer, CommentDeleteUpdateSerializer
 from comment.models import Comment
+from comment.api.paginations import CommentPagination
 
 class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
@@ -8,3 +10,26 @@ class CommentCreateAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
+
+class CommentListAPIView(ListAPIView):
+    serializer_class = CommentListSerializer
+    pagination_class = CommentPagination
+
+    def get_queryset(self):
+        queryset = Comment.objects.filter(parent=None)
+        query = self.request.GET.get("q")
+        if query:
+            queryset = queryset.filter(post = query)
+        return queryset
+
+class CommentDeleteAPIView(DestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentDeleteUpdateSerializer
+    lookup_field = 'pk'
+    permission_classes = [IsOwner]
+
+class CommentUpdateAPIView(UpdateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentDeleteUpdateSerializer
+    lookup_field = 'pk'
+    permission_classes = [IsOwner]
