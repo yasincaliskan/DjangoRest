e070e2a7064af6581ac7f48d035e259258c14e28
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-12-06 15:52:51 +0300

    Created Favourite App

---
+++ favourite/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+from post.models import Post
+
+class Favourite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=120)
