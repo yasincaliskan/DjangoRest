2d4e4a9263ad99a885e060cefdf8c99eb850fcd9
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-12-07 13:22:35 +0300

    Fixed permission

--- favourite/api/views.py
+++ favourite/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
 from favourite.api.paginations import FavouritePagination
 from favourite.api.permissions import IsOwner
 from favourite.api.serializers import FavouriteListCreateAPISerializer, FavouriteAPISerializer
@@ -8,6 +10,7 @@ from favourite.models import Favourite
 class FavouriteListCreateAPIView(ListCreateAPIView):
     serializer_class = FavouriteListCreateAPISerializer
     pagination_class = FavouritePagination
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Favourite.objects.filter(user = self.request.user)
