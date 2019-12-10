b7f907955b944253e047c9cff6081c22db6547fc
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-12-10 17:45:40 +0300

    Add favourite test

--- favourite/tests.py
+++ favourite/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework.utils import json
 
-# Create your tests here.
+from favourite.models import Favourite
+from post.models import Post
+
+
+class FavouriteCreateList(APITestCase):
+    url = reverse("favourite:list-create")
+    url_login = reverse("token_obtain_pair")
+
+    def setUp(self):
+        self.username = "yaskotest"
+        self.password = "test1234"
+        self.post = Post.objects.create(title="Header", content="Content-xxx")
+        self.user = User.objects.create_user(username = self.username, password = self.password)
+        self.test_jwt_authentication()
+
+    def test_jwt_authentication(self):
+        response = self.client.post(self.url_login, data = {'username': self.username, 'password': self.password})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("access" in json.loads(response.content))
+        self.token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+
+    #Add favourite
+    def test_add_favourite(self):
+        data = {
+            "content": "Nice Content",
+            "user": self.user.id,
+            "post": self.post.id
+        }
+
+        response = self.client.post(self.url, data)
+        self.assertEqual(201, response.status_code)
+
+    def test_user_favs(self):
+        self.test_add_favourite()
+        response = self.client.get(self.url)
+        self.assertTrue(len(json.loads(response.content)["results"]) == Favourite.objects.filter(user = self.user).count())
